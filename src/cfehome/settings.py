a67bb09aa5e4f2c4fdf3fd6907329ed847817782
Sam Damazio <samuel_damazio@hotmail.com>
2024-07-01 20:07:13 -0300

    PostgreSQL

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
+# import os
 from decouple import config
 from pathlib import Path
 
@@ -95,6 +95,18 @@ DATABASES = {
     }
 }
 
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
+DATABASE_URL = config("DATABASE_URL", cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_health_checks=True,
+            conn_max_age=CONN_MAX_AGE,
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
