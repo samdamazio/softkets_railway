6713979a45e3537b5fdda3ece789108c22b334f4
Sam Damazio <samuel_damazio@hotmail.com>
2024-07-06 16:15:17 -0300

    Added requirements for download static files and DJANGO_SECRET_KEY to dockerfile

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # my-apps
+    "commando",
     "visits",
 ]
 
@@ -144,6 +145,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_BASE_DIR = BASE_DIR / "staticfiles/"
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
 STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
 
 # source for python manage.py collectstatic
