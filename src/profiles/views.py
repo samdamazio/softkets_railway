bf6fd6af4320dcd646cce291a23d098143d78f8d
Sam Damazio <samuel_damazio@hotmail.com>
2024-08-15 16:19:06 -0300

    User Permissions in Django templates

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -5,6 +5,26 @@ from django.http import HttpResponse
 
 User = get_user_model()
 
+@login_required
+def profile_list_view(request):
+    context = {
+        "object_list": User.objects.filter(is_active=True)
+    }
+    
+    return render(request, "profiles/list.html", context)
+
+@login_required
+def profile_detail_view(request, username=None, *args, **kwargs):
+    user = request.user
+    profile_user_obj = get_object_or_404(User, username=username)
+    is_me = profile_user_obj == user
+    context = {
+        "object": profile_user_obj,
+        "instance": profile_user_obj,
+        "owner": is_me,
+    }
+    
+    return render(request, "profiles/detail.html", context)
 
 @login_required
 def profile_view(request, username=None, *args, **kwargs):
