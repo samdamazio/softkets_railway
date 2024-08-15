e143453bbfa4f05f5ea953023942ac328a50cb43
Sam Damazio <samuel_damazio@hotmail.com>
2024-08-15 15:51:17 -0300

    User Permissions and has_perm

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -9,6 +9,15 @@ User = get_user_model()
 @login_required
 def profile_view(request, username=None, *args, **kwargs):
     user = request.user
+
+    #<app_label>.view_<model_name>
+    #<app_label>.add_<model_name>
+    #<app_label>.change_<model_name>
+    #<app_label>.delete_<model_name>
+    
+    # print(user.has_perm("auth.view_user"))
+    # print(user.has_perm("visits.view_pagevisit"))
+
     # profile_user_obj = User.objects.get(username=username)
     profile_user_obj = get_object_or_404(User, username=username)
     is_me = profile_user_obj == user
